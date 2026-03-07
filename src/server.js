f64a8b1a14097698761856e470c55e87087273aa
Thang Nguyen <thangnv1029@gmail.com>
2026-03-07 14:05:36 +0700

    First commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from the medical chatbot backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
